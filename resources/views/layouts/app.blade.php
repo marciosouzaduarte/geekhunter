9034258c2064f979d51b458f3f0376b0c727bc1d
Marcio Souza Duarte <souzaduarte@gmail.com>
2021-05-29 20:49:23 -0300

    second commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,8 @@
                 @auth
                 <a class="navbar-brand" href="{{ url('/home') }}">Home</a>
                 <a class="navbar-brand" href="{{ url('/company') }}">Empresa</a>
+                @else
+                <a class="navbar-brand font-weight-bold" href="{{ url('/') }}">Laravel</a>
                 @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
